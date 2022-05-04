87c58ea1274044ce43594e96d8ea8a4a2e214e7f
oygull <oygulkhom3008@gmail.com>
2022-05-04 12:27:35 +0500

    changes

---
+++ js/main.js
@@ -0,0 +1,91 @@
+let firstInput = document.getElementById('firstInput');
+
+function setValue(){
+  firstInput.value= 'H'
+}
+function setValue2(){
+  firstInput.value= 'He'
+}
+function setValue3(){
+  firstInput.value= 'Hey'
+}
+function setValue4(){
+  firstInput.value= 'Hey #'
+}
+function setValue5(){
+  firstInput.value= 'Hey #f'
+}
+function setValue6(){
+  firstInput.value= 'Hey #fr'
+}
+function setValue7(){
+  firstInput.value= 'Hey #fro'
+}
+function setValue8(){
+  firstInput.value= 'Hey #fron'
+}
+function setValue9(){
+  firstInput.value= 'Hey #front'
+}
+function setValue10(){
+  firstInput.value= 'Hey #front'
+}
+function setValue11(){
+  firstInput.value= 'Hey #fronte'
+}
+function setValue12(){
+  firstInput.value= 'Hey #frontend'
+}
+function setValue13(){
+  firstInput.value= 'Hey #frontend d'
+}
+function setValue14(){
+  firstInput.value= 'Hey #frontend de'
+}
+function setValue15(){
+  firstInput.value= 'Hey #frontend dev'
+}
+function setValue16(){
+  firstInput.value= 'Hey #frontend deve'
+}
+function setValue17(){
+  firstInput.value= 'Hey #frontend devel'
+}
+function setValue18(){
+  firstInput.value= 'Hey #frontend develo'
+}
+function setValue19(){
+  firstInput.value= 'Hey #frontend develop'
+}
+function setValue20(){
+  firstInput.value= 'Hey #frontend develope'
+}
+function setValue21(){
+  firstInput.value= 'Hey #frontend developer'
+}
+function setValue22(){
+  firstInput.value= 'Hey #frontend developers!'
+}
+
+setTimeout(setValue, 1000);
+setTimeout(setValue2, 1500);
+setTimeout(setValue3, 2000);
+setTimeout(setValue4, 2500);
+setTimeout(setValue5, 3000);
+setTimeout(setValue6, 3500);
+setTimeout(setValue7, 4000);
+setTimeout(setValue8, 4500);
+setTimeout(setValue9, 5000);
+setTimeout(setValue10, 5500);
+setTimeout(setValue11, 6000);
+setTimeout(setValue12, 6500);
+setTimeout(setValue13, 7000);
+setTimeout(setValue14, 7500);
+setTimeout(setValue15, 8000);
+setTimeout(setValue16, 8500);
+setTimeout(setValue17, 9000);
+setTimeout(setValue18, 9500);
+setTimeout(setValue19, 10000);
+setTimeout(setValue20, 10500);
+setTimeout(setValue21, 11000);
+setTimeout(setValue22, 11500);
